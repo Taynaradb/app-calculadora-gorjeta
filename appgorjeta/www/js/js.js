3179d507afa39f6e9e05e8f4b14753ffc8643849
Taynara <taynaraax0@hotmail.com>
2022-06-01 22:05:04 -0300

    commit

---
+++ appgorjeta/www/js/js.js
@@ -0,0 +1,9 @@
+function calcularGorjeta(){
+    let valorTotal = document.querySelector("#valor").value;
+    valorGorjeta = valorTotal * 0.1;
+    document.querySelector("#resultado").innerHTML = "Gorjeta: R$ " + valorGorjeta;
+}
+
+document.querySelector("#btcalcular").addEventListener("click",function(){
+    calcularGorjeta();
+})
